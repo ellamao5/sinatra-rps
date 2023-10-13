311732e26fe5b54ca6c2776bb3d70099fb2ba1e4
ellamao5 <146144905+ellamao5@users.noreply.github.com>
2023-10-13 20:21:55 +0000

    rock

--- app.rb
+++ app.rb
@@ -7,3 +7,39 @@ get("/") do
   <p>Define some routes in app.rb</p>
   "
 end
+
+get("/rock") do
+  moves = ["rock", "paper", "scissors"]
+
+  @comp_move = moves.sample
+
+  if @comp_move == "rock"
+    @outcome = "tied"
+  elsif @comp_move == "paper"
+    @outcome = "lost"
+  elsif @comp_move == "scissors"
+    @outcome = "won"
+  end
+
+  erb(:zebra)
+end
+
+get("/scissors") do
+  moves = ["rock", "paper", "scissors"]
+ 
+  @comp_move = moves.sample
+
+  if @comp_move == "rock"
+    @outcome == "lost"
+  elsif @comp_move == "scissors"
+    @outcome == "tied"
+  elsif @cop_move == "paper"
+    @outcome == "won"
+  end
+
+  erb(:elephant)
+end
+
+get("/paper") do
+  erb(:giraffe)
+end
