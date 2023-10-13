5e16f17661f62abdc737bce7e2d1489a357e67d8
ellamao5 <146144905+ellamao5@users.noreply.github.com>
2023-10-13 20:49:23 +0000

    problem

--- app.rb
+++ app.rb
@@ -23,20 +23,32 @@ end
 
 get("/scissors") do
   moves = ["rock", "paper", "scissors"]
- 
-  @comp_move = moves.sample
 
-  if @comp_move == "rock"
-    @outcome == "lost"
-  elsif @comp_move == "scissors"
-    @outcome == "tied"
-  elsif @cop_move == "paper"
-    @outcome == "won"
+  comp_move = moves.sample
+  
+  if @comp_move = "rock"
+    @outcome = "lost"
+  elsif @comp_move = "scissors"
+    @outcome = "tied"
+  elsif @comp_move = "paper"
+      @outcome = "won"
   end
 
   erb(:elephant)
 end
 
 get("/paper") do
+  moves = ["rock", "paper", "scissors"]
+
+  comp_move = moves.sample
+
+  if @comp_move = "rock"
+    @outcome = "won"
+  elsif @comp_move = "scissors"
+    @outcome = "lost"
+  elsif @comp_move = "paper"
+    @outcome = "tied"
+  end
+
   erb(:giraffe)
 end
